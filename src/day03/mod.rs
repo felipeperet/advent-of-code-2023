f1070efe3ee9074190c563512cfd69bcd4303b6d
Felipe Sasdelli <sasdelli.pi@gmail.com>
2024-08-09 23:58:45 -0300

    Day 03 part 2

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -1,6 +1,7 @@
 use crate::day_trait::AdventDay;
 use regex::Regex;
 use std::{
+    collections::HashSet,
     fs::File,
     io::{self, BufRead, BufReader},
 };
@@ -76,11 +77,111 @@ impl AdventDay for Day03 {
             .sum();
 
         println!("Answer: {total_sum}");
-
         Ok(())
     }
 
     fn part2(&self) -> io::Result<()> {
+        let path = "src/day03/input.txt";
+        let file = File::open(path)?;
+        let read = BufReader::new(file);
+
+        let matrix: Vec<Vec<char>> = read
+            .lines()
+            .filter_map(Result::ok)
+            .map(|line| line.chars().collect())
+            .collect();
+
+        let directions = [
+            (-1, -1),
+            (-1, 0),
+            (-1, 1),
+            (0, -1),
+            (0, 1),
+            (1, -1),
+            (1, 0),
+            (1, 1),
+        ];
+        let re = Regex::new(r"[^a-zA-Z0-9.]").unwrap();
+
+        fn get_int(
+            cs: &Vec<char>,
+            col: usize,
+            row: usize,
+            seen_positions: &mut HashSet<(usize, usize)>,
+        ) -> i32 {
+            let mut number: Vec<char> = Vec::new();
+            let mut index = col;
+
+            while cs[index].is_numeric() {
+                if !seen_positions.insert((row, index)) {
+                    break;
+                }
+
+                number.insert(0, cs[index]);
+
+                if index == 0 {
+                    break;
+                }
+
+                index -= 1;
+            }
+
+            index = col + 1;
+
+            while index < cs.len() && cs[index].is_numeric() {
+                if !seen_positions.insert((row, index)) {
+                    break;
+                }
+
+                number.push(cs[index]);
+                index += 1;
+            }
+
+            number.iter().collect::<String>().parse().unwrap_or(0)
+        }
+
+        let mut adjacent_numbers: Vec<i32> = Vec::new();
+
+        for (i, row) in matrix.iter().enumerate() {
+            let row_str: String = row.iter().collect();
+
+            for mat in re.find_iter(&row_str) {
+                let col = mat.start();
+
+                let mut seen_positions: HashSet<(usize, usize)> = HashSet::new();
+                let mut counter = 0;
+
+                directions.iter().for_each(|(dx, dy)| {
+                    let posx = (i as isize + dx) as usize;
+                    let posy = (col as isize + dy) as usize;
+
+                    if posx < matrix.len() && posy < matrix[posx].len() {
+                        if matrix[posx][posy].is_numeric()
+                            && !seen_positions.contains(&(posx as usize, posy as usize))
+                        {
+                            let number = get_int(&matrix[posx], posy, posx, &mut seen_positions);
+
+                            adjacent_numbers.push(number);
+                            counter += 1;
+                        }
+                    }
+                });
+                if counter != 2 {
+                    for _ in 1..=counter {
+                        adjacent_numbers.pop();
+                    }
+                }
+            }
+        }
+
+        let mut sum = 0;
+        let mut i = 0;
+        while i < adjacent_numbers.len() {
+            sum += adjacent_numbers[i] * adjacent_numbers[i + 1];
+            i += 2;
+        }
+
+        println!("Answer: {sum}");
         Ok(())
     }
 }
