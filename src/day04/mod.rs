2ca9160791ae075ebda26248248a8a3a9049aed9
Felipe Sasdelli <sasdelli.pi@gmail.com>
2024-08-11 11:20:35 -0300

    Day 04

---
+++ src/day04/mod.rs
@@ -0,0 +1,14 @@
+use crate::day_trait::AdventDay;
+use std::io::{self};
+
+pub struct Day04;
+
+impl AdventDay for Day04 {
+    fn part1(&self) -> io::Result<()> {
+        Ok(())
+    }
+
+    fn part2(&self) -> io::Result<()> {
+        Ok(())
+    }
+}
