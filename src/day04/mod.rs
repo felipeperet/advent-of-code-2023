aa4f53494456cfc83f362e52f70a9e00d34a9541
Felipe Sasdelli <sasdelli.pi@gmail.com>
2024-10-07 13:31:16 -0300

    Day4 part2 (failing input)

--- src/day04/mod.rs
+++ src/day04/mod.rs
@@ -2,6 +2,7 @@ use regex::Regex;
 
 use crate::day_trait::AdventDay;
 use std::{
+    collections::{HashMap, HashSet, VecDeque},
     fs::File,
     io::{self, BufRead, BufReader},
 };
@@ -24,7 +25,8 @@ impl AdventDay for Day04 {
         let file = File::open(path)?;
         let read = BufReader::new(file);
 
-        let re = Regex::new(r":\s([\d\s]+)\|([\d\s]+)").unwrap();
+        // Example of Regex Match: Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        let re = Regex::new(r"([\d\s]+)\|([\d\s]+)").unwrap();
         let mut total = 0;
 
         for line in read.lines() {
@@ -56,6 +58,59 @@ impl AdventDay for Day04 {
     }
 
     fn part2(&self) -> io::Result<()> {
+        let path = "src/day04/input.txt";
+        let file = File::open(path)?;
+        let read = BufReader::new(file);
+
+        let mut card_multipliers: HashMap<i32, i32> = HashMap::new();
+
+        // Example of Regex Match: Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        let re = Regex::new(r"Card\s(\d+):\s([\d\s]+)\|([\d\s]+)").unwrap();
+
+        for (i, line) in read.lines().enumerate() {
+            let line = line?;
+            println!("Processing line {}: {}", i + 1, line);
+
+            card_multipliers.entry(i as i32 + 1).or_insert(1);
+
+            if let Some(caps) = re.captures(&line) {
+                let card_number: i32 = caps[1].parse().unwrap();
+                let winning_numbers: Vec<i32> = parse_numbers(caps.get(2));
+                let received_numbers: Vec<i32> = parse_numbers(caps.get(3));
+
+                println!(
+                    "Card number: {}, Winning numbers: {:?}, Received numbers: {:?}",
+                    card_number, winning_numbers, received_numbers
+                );
+
+                let points: i32 = received_numbers
+                    .iter()
+                    .filter(|n| winning_numbers.contains(n))
+                    .count() as i32;
+
+                println!("Points: {}", points);
+
+                for i in 1..=points {
+                    card_multipliers.entry(card_number + i).or_insert(1);
+
+                    card_multipliers.insert(
+                        card_number + i,
+                        card_multipliers[&(card_number + i)] + card_multipliers[&card_number],
+                    );
+
+                    println!(
+                        "Updated card multiplier for card {}: {}",
+                        card_number + i,
+                        card_multipliers[&(card_number + i)]
+                    );
+                }
+            }
+        }
+
+        let total_value_sum: i32 = card_multipliers.values().sum();
+        println!("Total sum of card multipliers: {}", total_value_sum);
+
+        println!("Answer: {total_value_sum}");
         Ok(())
     }
 }
